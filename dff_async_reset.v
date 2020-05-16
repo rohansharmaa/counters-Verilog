b2b84905cf3bd2aae07327ceb79824b0524910ac
Rohan Sharma <rohansharma573@gmail.com>
2020-05-16 21:54:03 +0530

    Update dff_async_reset.v

--- dff_async_reset.v
+++ dff_async_reset.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/04/2018 01:53:20 PM
-// Design Name: 
-// Module Name: dff_async_reset
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module dff_async_reset( d,clk,rst,q );
